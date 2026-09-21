3f286eb503e08e52720129d4ac087f65058b4d56
Lunaproject1 <jggpt1989@gmail.com>
2026-09-22 02:04:26 +0300

    Refresh Notes PWA cache after icon alignment

--- notes-v01-sw.js
+++ notes-v01-sw.js
@@ -1,4 +1,4 @@
-const CACHE='luna-notes-v013-pwa3';
+const CACHE='luna-notes-v013-pwa4';
 const ASSETS=['./notes-v013.html','./notes-v01.webmanifest','./notes-icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('luna-notes-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
