c5931da9cdfe0e34129f9f8328f359062f679a42
Lunaproject1 <jggpt1989@gmail.com>
2026-09-22 01:58:25 +0300

    Refresh Notes PWA cache for in-app logo

--- notes-v01-sw.js
+++ notes-v01-sw.js
@@ -1,4 +1,4 @@
-const CACHE='luna-notes-v013-pwa1';
+const CACHE='luna-notes-v013-pwa2';
 const ASSETS=['./notes.html','./notes-v013.html','./notes-v01.webmanifest','./notes-icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('luna-notes-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
