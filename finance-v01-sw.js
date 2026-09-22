238fafc90bbc969f27140a518651d1b784b134de
Lunaproject1 <jggpt1989@gmail.com>
2026-09-22 11:45:58 +0300

    Refresh Finance cache for fee grouping and emergency wipe

--- finance-v01-sw.js
+++ finance-v01-sw.js
@@ -1,4 +1,4 @@
-const C='luna-finance-v0298-pwa2-hotfix1';
+const C='luna-finance-v0298-pwa2-hotfix2';
 const A=['./finance-v0298.html','./finance-v01.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x.startsWith('luna-finance-')&&x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
