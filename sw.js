b8fc0b79a17de24b1b86b72c436ac235a5c5fdd9
Lunaproject1 <jggpt1989@gmail.com>
2026-09-22 14:49:56 +0300

    Isolate app releases from shared Core activation

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE='luna-suite-v10';
+const CACHE='luna-suite-v11';
 const APPS=['./shopping.html','./notes-v013.html','./finance-v0298.html'];
-const CORE=['./index.html','./luna-core.js',...APPS,'./manifest.webmanifest','./notes-v01.webmanifest','./finance-v01.webmanifest','./icon.svg','./notes-icon.svg','./shopping-icon.svg?v=3'];
+const CORE=['./index.html','./luna-core.js','./manifest.webmanifest','./notes-v01.webmanifest','./finance-v01.webmanifest','./icon.svg','./notes-icon.svg','./shopping-icon.svg?v=3'];
 
 self.addEventListener('install',event=>{
  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()));
@@ -11,8 +11,6 @@ self.addEventListener('activate',event=>{
   const keys=await caches.keys();
   await Promise.all(keys.filter(key=>(key.startsWith('luna-suite-')||key.startsWith('luna-compras-')||key.startsWith('luna-notes-')||key.startsWith('luna-finance-'))&&key!==CACHE).map(key=>caches.delete(key)));
   await self.clients.claim();
-  const clients=await self.clients.matchAll({type:'window',includeUncontrolled:true});
-  for(const client of clients){try{await client.navigate(client.url)}catch(_){}}
  })());
 });
 
